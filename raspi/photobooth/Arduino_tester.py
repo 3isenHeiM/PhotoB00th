3c112708f14b1a1a0d7a4016a6491f4241c1917c
3isenHeiM <pierre.vivegnis@belgacom.net>
2019-01-04 12:32:42 +0100

    Added available options, UTF-8 decoding, and readline() utilization.

--- raspi/photobooth/Arduino_tester.py
+++ raspi/photobooth/Arduino_tester.py
@@ -4,18 +4,10 @@
 import os
 import sys
 import time
-import signal
-import traceback
-import logging
-from logging import handlers
-import argparse
-import gzip
-import gphoto2 as gp
 import serial
-import RPi.GPIO as GPIO
 
 # Setup the serial port
-ser = serial.Serial(
+arduino = serial.Serial(
     port='/dev/ttyUSB1',
     baudrate=9600,
     parity=serial.PARITY_ODD,
@@ -24,30 +16,43 @@ ser = serial.Serial(
 )
 
 # Try to open port
-ser.isOpen()
+arduino.isOpen()
 print("Serial communication opened to: %s" %ser.portstr)
 
+# Print available commands
+print """Enter your commands below. Available commands for the arduino photobooth :
+    - Digits from 0 to 9
+    - displayOff
+    - spotsOn
+    - spotsOff
+    - spotsDimm
+    - S<X>_on to light sport <X>
+    - S<X>_off to light out sport <X>
+    - animation1
+    - animation2
+    - animationSpots
+    - clear
+Insert "exit" to leave the application.
+"""
 
-print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
-
-input=1
 while 1 :
     # get keyboard input
-    input = raw_input(">> ")
+    command = raw_input(">> ")
         # Python 3 users
         # input = input(">> ")
-    if input == 'exit':
-        ser.close()
+    if command == 'exit':
+        arduino.close()
         exit()
     else:
         # send the character to the device
-        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-        ser.write(input + '\r\n')
+        arduino.write(command + '\r\n')
+
         out = ''
         # let's wait one second before reading output (let's give device time to answer)
         time.sleep(1)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
+        while arduino.inWaiting() > 0:
+            # Read a line, decode it as UTF-8 (used by Arduino) and remove \n\r
+            out = arduino.readline().decode("utf-8").strip('\n').strip('\r')
 
         if out != '':
-          print ">>" + out
+          print "--> " + out
