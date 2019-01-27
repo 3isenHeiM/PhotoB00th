9d3e739f1f1720258a005951117b28dc54591b90
3isenHeiM <pierre.vivegnis@belgacom.net>
2019-01-27 10:07:02 +0100

    Added error handling, and removed the wait before reading the command.

--- raspi/photobooth/Arduino_tester.py
+++ raspi/photobooth/Arduino_tester.py
@@ -6,18 +6,27 @@ import sys
 import time
 import serial
 
-# Setup the serial port
-arduino = serial.Serial(
-    port='/dev/ttyUSB1',
-    baudrate=9600,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS
-)
 
-# Try to open port
-arduino.isOpen()
-print("Serial communication opened to: %s" %ser.portstr)
+try:
+    # Setup the serial port
+    arduino = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=115200,
+        parity=serial.PARITY_ODD,
+        stopbits=serial.STOPBITS_TWO,
+        bytesize=serial.SEVENBITS
+    )
+
+    arduino.open()
+    if arduino.isOpen() : # Returns true if serial port is opened
+        print("Serial communication opened to: %s" %arduino.portstr)
+
+except Exception as e :
+    print("%s recieved, trying to open the port in 2s" %e)
+    arduino.close()
+    time.sleep(2)
+    arduino.open()
+    print("Port was already open, was closed and opened again.")
 
 # Print available commands
 print """Enter your commands below. Available commands for the arduino photobooth :
@@ -49,7 +58,7 @@ while 1 :
 
         out = ''
         # let's wait one second before reading output (let's give device time to answer)
-        time.sleep(1)
+        #time.sleep(1)
         while arduino.inWaiting() > 0:
             # Read a line, decode it as UTF-8 (used by Arduino) and remove \n\r
             out = arduino.readline().decode("utf-8").strip('\n').strip('\r')
